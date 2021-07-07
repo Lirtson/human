f6befc2a3db5c1a14886acfb37fb966e953fa512
Lirson <madison00@126.com>
2021-07-07 11:25:32 +0800

    增加登录功能

--- src/main/java/com/liang/human/service/UserService.java
+++ src/main/java/com/liang/human/service/UserService.java
@@ -1,6 +1,9 @@
 package com.liang.human.service;
 
 import com.liang.human.dao.UserDao;
+import com.liang.human.domain.User;
+import com.liang.human.exception.CustomException;
+import com.liang.human.exception.CustomExceptionType;
 import com.liang.human.model.UserVO;
 import com.liang.human.utils.JWTUtils;
 import org.springframework.stereotype.Service;
@@ -42,6 +45,28 @@ public class UserService {
         } catch (Exception e) {
             throw new RuntimeException("创建Token失败");
         }
+        return token;
+    }
+    //登录，返回token
+    public String login(UserVO userVo) {
+        if(userVo == null) {
+            throw new CustomException(CustomExceptionType.SYSTEM_ERROR,"服务端异常");
+        }
+        User user = userDao.getByName(userVo.getUsername());
+        if(user == null) {
+            throw new CustomException(CustomExceptionType.NOT_LOGIN,"账号不存在");
+        }
+        //验证密码
+        String inputPass=userVo.getPassword();
+        int inputRole=userVo.getRole();
+        String password=user.getPassword();
+        int role=user.getRole();
+        if(!(inputPass.equals(password))||!(inputRole==role)) {
+            throw new CustomException(CustomExceptionType.NOT_LOGIN,"密码或角色错误");
+        }
+        long userId=user.getId();//获取用户id
+        //生成token
+        String token= getToken(userId);
         return token;
     }
 }
